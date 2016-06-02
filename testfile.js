86847381d9e3b24040c2de59187a2f00d050e4e5
MSH <MrSherlockHolmes123@gmail.com>
2016-06-02 15:44:52 +0100

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -9,35 +9,35 @@ var cloud;
 
 
 function encrypt(data) {
-	var chars = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~�"
-	var output = "";
-	var charIndex;
-	for (var i = 0; i < data.length; i++) {
-		charIndex = (chars.indexOf(data[i])+1).toString();
-		if (charIndex.length == "1") {
-			output += "0" + charIndex;
-		} else {
-			output += charIndex;
-		}
-	}
-	return output + "00";
+  var chars = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~�"
+  var output = "";
+  var charIndex;
+  for (var i = 0; i < data.length; i++) {
+    charIndex = (chars.indexOf(data[i])+1).toString();
+    if (charIndex.length == "1") {
+      output += "0" + charIndex;
+    } else {
+      output += charIndex;
+    }
+  }
+  return output + "00";
 }
 
 function runGenerator() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "https://randomuser.me/api/", true);
-	xhttp.send();
-	cloud.set("\u2601 randomUser", encrypt(xhttp.responseText));
-	setTimeout(runGenerator,10000);
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "https://randomuser.me/api/", true);
+  xhttp.send();
+  cloud.set("\u2601 randomUser", encrypt(JSON.stringify(xhttp.responseText)));
+  setTimeout(runGenerator, 10000);
 }
 
 Scratch.UserSession.create(username, password, function(e, user) {
-	if (e) { return console.error(e); }
-	console.log("Logged in.");
-	user.cloudSession(project_id, function(e, cloud_) {
-		if (e) { return console.error(e); }
-		console.log("Cloud connected.")
-		cloud = cloud_;
-		runGenerator();
-	});
+  if (e) { return console.error(e); }
+  console.log("Logged in.");
+  user.cloudSession(project_id, function(e, cloud_) {
+    if (e) { return console.error(e); }
+    console.log("Cloud connected.")
+    cloud = cloud_;
+    runGenerator();
+  });
 });
